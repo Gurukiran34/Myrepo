5466447c7d999b3170e2e97a3e32f2ec247ff5b9
Gurukiran34 <144530870+Gurukiran34@users.noreply.github.com>
2023-09-09 16:45:10 +0530

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+
+public class MyFirstJavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.SIZE/8);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+	}
+}
